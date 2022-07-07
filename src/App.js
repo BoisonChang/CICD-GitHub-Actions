4275e22969eaf0ddacfa9226f74920fef6d2fc81
boison.chang <boison.chang@flowring.com>
2022-07-07 18:19:48 +0800

    time

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn CICD by GitHub Actions 18:08
+          Learn CICD by GitHub Actions 18:19
         </a>
       </header>
     </div>
